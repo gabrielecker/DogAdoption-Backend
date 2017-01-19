299524238ec52b559a4d4686440eff66b177d11e
gabrielecker <gabriel.ecker@gmail.com>
2017-01-19 13:46:52 -0200

    Updating database url to use environment variable

--- project/config.py
+++ project/config.py
@@ -1,10 +1,11 @@
 # encodingg: utf-8
 import datetime
+import os
 
 
 class BaseConfig(object):
     SECRET_KEY = '71bcdb52a2cb4b09a5ddf6af4bf84e64'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost/dog_adoption'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     DEBUG = False
     REMEMBER_COOKIE_DURATION = datetime.timedelta(hours=24)
